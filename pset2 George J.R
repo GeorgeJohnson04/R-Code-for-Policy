60430bd418eb2156d74c6ee9566cfdad12040a98
George Johnson <151577251+GeorgeJohnson04@users.noreply.github.com>
2024-10-31 14:59:21 -0500

    Add files via upload

---
+++ pset2 George J.R
@@ -0,0 +1,37 @@
+#Name: George Johnson
+#ID: 2134609
+
+#Load libraries
+library(utils)
+library(readr)
+
+#Load data set
+india <- read_csv("C:/Users/Tommy_w7c1d3j/Desktop/School/Public Policy Stats/DSS/india.csv")
+View(india)
+
+#mean of female leaders
+mean(india$female)
+#The output tells us about 33.54037% or 33.5% of the villages have female leaders
+
+#mean of Irrigation
+mean(india$irrigation)
+#The output tells us that if we choose a random village we are likely to see 3.263975 new or repaired irrigation systems 
+#however since irrigation systems have to be represented by a whole number we can assume if we choose a random village there will be 3 irrigation systems new / repaired
+
+#Q3
+  #a.
+  #b.
+#Q4
+  #a.
+  #b.
+#Q5
+  #a.
+  #b.
+#Q6
+  #
+#Q7
+  #
+#Q8
+  #
+#Q9
+  #
